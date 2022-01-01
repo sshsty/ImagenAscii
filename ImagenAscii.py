8904653d5d71726da8f302b7213d2e4722e4c358
Demian <94668044+DezzmainOsKreuz@users.noreply.github.com>
2021-12-31 20:39:01 -0800

    Update ImagenAscii.py

--- ImagenAscii.py
+++ ImagenAscii.py
@@ -1,4 +1,4 @@
 from image_to_ascii import ImageToAscii
 
 pedir_ruta = input(print("Ingresa la ruta de su imagen con la extencion png o jpg: "))
-ImageToAscii(imagePath=pedir_ruta, outputFile="output.txt")
+ImageToAscii(imagePath=pedir_ruta,outputFile="output.txt")
